85f29e416a9084540112001232e357759cfb4556
Michael Hughes <mhughes36@live.co.uk>
2024-07-11 15:04:26 +0100

    Change env var

--- components/ProductsArea.tsx
+++ components/ProductsArea.tsx
@@ -2,7 +2,7 @@ import SearchComponent from '../components/SearchComponent'
 
 const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_VERCEL_URL
+    ? process.env.VERCEL_URL
     : "localhost:3000";
 
 export const getProducts = async () => {
